7868c50c09e12753ff39f80e62b1758aa0cd4111
Renan Ferreira <renanfc.re@gmail.com>
2024-11-04 13:41:52 -0300

    feat(docker-compose): adicionado configuração de rede

--- wait-for-mongo.sh
+++ wait-for-mongo.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-
-# Esperar até que o MongoDB esteja disponível
-until nc -z -v -w30 mongo 27017
-do
-  echo "Aguardando o MongoDB iniciar..."
-  sleep 1
+until wget -q --spider mongo:27017; do
+    echo "Aguardando o MongoDB iniciar..."
+    sleep 1
 done
-
 echo "MongoDB está disponível. Iniciando a aplicação..."
 exec "$@"
